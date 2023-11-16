1956dc9ba0a294cc23bb064275fab0a7dd85db5b
Pierrette Mugisha <mupierrix@yahoo.fr>
2023-11-16 16:28:41 -0500

    Implement the wc -c option to return the byte counts

---
+++ ccwc.py
@@ -0,0 +1,35 @@
+import sys
+import argparse
+
+
+def wc(filename, counts):
+    try:
+        with open(filename, 'rb') as file:
+
+            content = file.read()
+
+            if counts:
+                count = len(content)
+            else:
+                print(f'Invalid option')
+
+            print(f'{count} {filename}')
+
+    except FileNotFoundError:
+        print(f'Error: File {filename} not found')
+    except Exception as e:
+        print(f'An error occurred {e}')
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Custom Word Count Tool")
+    parser.add_argument('filename', help='File to analyse')
+    parser.add_argument('-c', '--bytes', action='store_true', help='print the byte counts')
+    args = parser.parse_args()
+
+    if args.bytes:
+        wc(args.filename, '-c')
+
+
+if __name__ == "__main__":
+    main()
